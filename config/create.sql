8c3b7865ddda5c6f9fc71193c0150c9e6be1455b
Willian Santa Ana <willian.ana@pdmfc.com>
2025-03-15 11:38:04 +0000

    update create sql script

--- config/create.sql
+++ config/create.sql
@@ -3,12 +3,14 @@ BEGIN;
 CREATE TABLE IF NOT EXISTS tweets
 (
     id serial NOT NULL,
-    pub_id bigint NOT NULL,
+    symbol varchar(255) NOT NULL,
+    pub_id bigint NOT NULL UNIQUE,
     pub_author varchar(255) NOT NULL,
     pub_text text NOT NULL,
     pub_img text,
-    pub_date time NOT NULL,
-    symbol varchar(255),
+    pub_img_text text,
+    pub_img_caption text,
+    pub_date timestamp NOT NULL,
     sentiment varchar(255),
     PRIMARY KEY (id)
 );
